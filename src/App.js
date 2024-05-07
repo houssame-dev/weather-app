c4660a273b7df186b309dc8482fceee29ea376a8
houssame errjem <errjem2@gmail.com>
2024-05-07 22:35:30 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ function App() {
   return (
     <div className="App">
       <WeatherApp />
-      <iframe
-        title="panel"
-        src="http://localhost:3000/d-solo/8jXBdLfIk/flespi-dashboard?orgId=1&refresh=5s&from=1715030967116&to=1715117367117&var-Vehicle=TOUAREG%20%235619757&panelId=4"
-        width="450"
-        height="200"
-        frameborder="0"
-      ></iframe>
     </div>
   );
 }
